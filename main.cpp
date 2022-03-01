803fadceaa207fff78e9521f96a7962c1b6624bb
Swaroop Joshi <swaroopj@goa.bits-pilani.ac.in>
2022-03-01 13:41:15 +0530

    Add program

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
-using namespace std;
+void my_assert(int expected, int actual) {
+  if (expected == actual) std::cout << "OK" << std::endl;
+  else std::cerr << "Expected :" << expected << ", actual: " << actual << std::endl;
+}
+
+int sum(int a, int b) {
+  // TODO: implement function
+  return 0; // placeholder
+}
 
 int main() {
-
-    return 0;
+  my_assert(5, sum(2, 3));
+  my_assert(0, sum(1, -1));
+  
+  return 0;
 }
